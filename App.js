f1053d9b4d65a39b127048a77a99ce94848655ca
dameyerdave <davmeyer@nexus-mac-11.home>
2020-07-24 07:57:59 +0200

    corrected export issues

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { Slider } from './src/Slider'
+import { Slider } from './src/index'
 
 export default function App() {
   const [level, setLevel] = useState(2)
